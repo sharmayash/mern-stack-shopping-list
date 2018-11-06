33f33b3f614c4e37e519e746ac372f7e2e82998e
yash sharma <sharmayash44ys@gmail.com>
2018-11-06 23:46:26 +0530

    ready for deploy

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const items = require('./routes/api/items');
+const path = require('path');
 
 const app = express();
 
@@ -17,6 +18,13 @@ mongoose
 
 // --------Use-Routes----------
 app.use('/api/items', items);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('cilent/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 
